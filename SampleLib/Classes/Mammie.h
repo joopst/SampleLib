f41661a264fc5884ce735de162d8291c9011dad7
Joost de Moel <joost@ngti.nl>
2015-07-16 15:39:54 +0200

    mammie file

---
+++ SampleLib/Classes/Mammie.h
@@ -0,0 +1,15 @@
+//
+//  Mammie.h
+//  Pods
+//
+//  Created by Joost de Moel on 16/07/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mammie : NSObject
+
+- (void)braak;
+
+@end
---
+++ SampleLib/Classes/Mammie.m
@@ -0,0 +1,19 @@
+//
+//  Mammie.m
+//  Pods
+//
+//  Created by Joost de Moel on 16/07/15.
+//
+//
+
+#import "Mammie.h"
+
+@implementation Mammie
+
+- (void)braak
+{
+    int i = 0;
+    i++;
+}
+
+@end
