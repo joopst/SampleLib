d53ada29b9d9318ede40274e89ebc12fe36e353b
Joost de Moel <joost@ngti.nl>
2015-07-16 13:29:28 +0200

    my handy lib

---
+++ SampleLib/SampleLib.h
@@ -0,0 +1,13 @@
+//
+//  SampleLib.h
+//  SampleLib
+//
+//  Created by Joost de Moel on 16/07/15.
+//  Copyright (c) 2015 Joost de Moel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleLib : NSObject
+
+@end
---
+++ SampleLib/SampleLib.m
@@ -0,0 +1,13 @@
+//
+//  SampleLib.m
+//  SampleLib
+//
+//  Created by Joost de Moel on 16/07/15.
+//  Copyright (c) 2015 Joost de Moel. All rights reserved.
+//
+
+#import "SampleLib.h"
+
+@implementation SampleLib
+
+@end
