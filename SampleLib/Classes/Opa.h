c073b8f431f51c4e518d7c803691f3d0f8a6cc84
Joost de Moel <joost@ngti.nl>
2015-07-16 15:00:02 +0200

    Final Release for 0.0.1

---
+++ SampleLib/Classes/Opa.h
@@ -0,0 +1,13 @@
+//
+//  Opa.h
+//  Pods
+//
+//  Created by Joost de Moel on 16/07/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Opa : NSObject
+
+@end
---
+++ SampleLib/Classes/Opa.m
@@ -0,0 +1,13 @@
+//
+//  Opa.m
+//  Pods
+//
+//  Created by Joost de Moel on 16/07/15.
+//
+//
+
+#import "Opa.h"
+
+@implementation Opa
+
+@end
